de489e2ef7b03efb12c28e8a6ac008b0602b9508
伊東和成 <itokazunari5375@icloud.com>
2022-03-13 22:57:05 +0900

    foodsのapiを作成

---
+++ app/controllers/api/v1/foods_controller.rb
@@ -0,0 +1,12 @@
+module Api
+  module V1
+    class FoodsController < ApplicationController
+      def index
+        foods = Food.all
+        render json: {
+          foods:  foods
+        }, status: :ok
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # --- ここから追加 ---
+  namespace :api do
+    namespace :v1 do
+      resources :restaurants do
+        resources :foods, only:%i[index]
+      end
+    end
+  end
+  # --- ここまで追加 ---
 end
