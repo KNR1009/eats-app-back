e05c42c3ec637a9c3087c710ec84b818eeacee4e
伊東和成 <itokazunari5375@icloud.com>
2022-03-20 22:31:02 +0900

    商品の詳細を取得

--- app/controllers/api/v1/foods_controller.rb
+++ app/controllers/api/v1/foods_controller.rb
@@ -1,16 +1,28 @@
 module Api
   module V1
     class FoodsController < ApplicationController
+      before_action :restaurant_param, only: [:show]
+      # 店舗の商品一覧
       def index
-        foods = Food.all
-        render json: {
-          foods:  foods
+        restaurant = Restaurant.find(params[:restaurant_id])
+        foods = restaurant.foods
+        render json:{
+          foods: foods
         }, status: :ok
       end
 
+      def show
+        render json: {
+          food: @food
+        }, status: :ok
+      end
+
+      private
+      def restaurant_param
+        restaurant = Restaurant.find(params[:restaurant_id])
+        @food = restaurant.foods.find(params[:id])
+      end
+
     end
-
-    private
-
   end
 end
